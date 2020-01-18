867a15ea75aa977a20a991bbadac6607b6fc640d
Ku-git <r0905121198@gmail.com>
2020-01-19 01:21:06 +0800

    git init commit

---
+++ src/test/java/com/ku/api/RestfulApIdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ku.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApIdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
